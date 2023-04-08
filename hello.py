79daca9a5c66aa660409f96536f36f2e85924020
pandemonium0225 <pandemonium0225@gmail.com>
2023-04-08 19:25:22 +0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    first_name = "John"
+    favorite_pizza = ['Pepperoni','Cheese','Supreme','Mushroom']
+    return render_template('index.html', 
+                            f_name=first_name,
+                            favorite_pizza = favorite_pizza
+                            )
+@app.route('/about')
+def about():
+    return render_template('about.html')
